89377dfb04a5e76c4418d658ed8d8e4abfd958c1
SachaYT1 <sacha2005@bk.ru>
2026-04-03 19:48:50 +0300

    fix cassandra

--- app/store_index.sh
+++ app/store_index.sh
@@ -6,7 +6,7 @@ source .venv/bin/activate
 export PYSPARK_DRIVER_PYTHON=$(which python)
 export PYSPARK_PYTHON=./.venv/bin/python
 
-spark-submit --master yarn --archives /app/.venv.tar.gz#.venv store_index.py
+spark-submit --master local[*] store_index.py
 
 if [ $? -ne 0 ]; then
     echo "ERROR: Failed to store index in Cassandra"
