757e578aa11ab3ba386b911c0562d71233bebdaa
SachaYT1 <sacha2005@bk.ru>
2026-04-03 19:29:58 +0300

    fix build

--- app/app.sh
+++ app/app.sh
@@ -5,12 +5,16 @@ service ssh restart
 # Starting the services
 bash start-services.sh
 
+# Install build dependencies
+apt-get update && apt-get install -y python3-dev build-essential
+
 # Creating a virtual environment
 python3 -m venv .venv
 source .venv/bin/activate
 
 # Install any packages
-pip install -r requirements.txt  
+pip install --upgrade pip setuptools wheel
+pip install -r requirements.txt
 
 # Package the virtual env.
 venv-pack -o .venv.tar.gz
